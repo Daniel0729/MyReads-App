f3bb772334868b371e09504816a0f5e39dd4e1cf
songnian yin <syin2@stevens.edu>
2017-10-16 17:34:38 -0400

    feat:add searchBook function

--- src/compoments/searchBook.js
+++ src/compoments/searchBook.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import * as BooksAPI from '../BooksAPI';
-import BookShelf from './bookShelf';
 import '../App.css';
 
 class SearchBook extends React.Component {
+    state = {
+        searchBooks:[]
+    }
+
+    searchBook =(query) => {
+        console.log(query);
+        console.log(query.length);
+        BooksAPI.search(query).then(Response => {
+            if (!Response.error) {
+                console.log(Response);
+                this.setState({
+                    searchBooks: Response
+                })
+            }
+            else {
+                console.log(Error(`something wrong`));
+                this.setState({
+                    searchBooks:[]
+                })
+                
+            }
+        })
+    }
     render() {
         return (
             <div className="search-books">
@@ -18,11 +40,36 @@ class SearchBook extends React.Component {
                         However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                         you don't find a specific author or title. Every search is limited by search terms.
                     */}
-                    <input type="text" placeholder="Search by title or author"/>
+                    <input type="text" placeholder="Search by title or author" onChange={event=>this.searchBook(event.target.value)}/>
                     </div>
                 </div>
                 <div className="search-books-results">
-                    <ol className="books-grid">books</ol>
+                    <ol className="books-grid">
+                        {(this.state.searchBooks.length !== 0) ? (this.state.searchBooks.map((book) => (
+                            <li key={book.id}>
+                                <div className="book">
+                                    <div className="book-top">
+                                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+                                    <div className="book-shelf-changer">
+                                        <select defaultValue={book.shelf} onChange={(event) => this.props.onUpdateShelf(event.target.value,book)}>
+                                        <option value="none" disabled>Move to...</option>
+                                        <option value="currentlyReading">Currently Reading</option>
+                                        <option value="wantToRead">Want to Read</option>
+                                        <option value="read">Read</option>
+                                        <option value="none">None</option>
+                                        </select>
+                                    </div>
+                                    </div>
+                                    <div className="book-title">{book.title}</div>
+                                    <div className="book-authors">{book.authors}</div>
+                                </div>
+                            </li>))) : (
+                                <div>
+                                    en... try to input another title or author
+                                </div>
+                            )
+                        }
+                    </ol>
                 </div>
             </div>
         )
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import BooksApp from './compoments/App'
+//import BooksApp from './compoments/App'
 import SearchBook from './compoments/searchBook'
 import './index.css'
 
